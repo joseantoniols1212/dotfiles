888c36df0fdc87fe07220d8ae555eb2b20de9ae6
Jose Antonio Luque <joseantonioluquesalguero1212@gmail.com>
2024-11-10 15:50:45 +0100

    wallpapers, nerdfonts

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Archivo de instalacion de mi configuracion (para ubuntu)
 
-# Versión de Neovim a instalar
-NEOVIM_VERSION="v0.10.2"
-
 # Función para comprobar e instalar un paquete por apt
 instalar_paquete() {
   local PAQUETE=$1
@@ -13,7 +10,7 @@ instalar_paquete() {
     echo "El paquete '$PAQUETE' ya está instalado."
   else
     echo "El paquete '$PAQUETE' no está instalado. Procediendo con la instalación..."
-    sudo apt install -y "$PAQUETE"
+    sudo apt install -y "$PAQUETE" -qq > /dev/null 2>&1
 
     # Verificar si la instalación fue exitosa
     if dpkg -l | grep -qw "$PAQUETE"; then
@@ -25,22 +22,73 @@ instalar_paquete() {
   fi
 }
 
+########################################
 # Actualizamos repositorios y paquetes
+########################################
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
-# Instalamos paquetes
+####################################
+# Instalamos paquetes generales
+####################################
 instalar_paquete git
-instalar_paquete curl
-instalar_paquete build-essential
+instalar_paquete curl                        # herramienta para descargar desde internet
+instalar_paquete build-essential             # instala gcc, g++...
 instalar_paquete python3
-instalar_paquete kitty
-instalar_paquete zsh
+instalar_paquete kitty                       # terminal
+instalar_paquete zsh                         # shell
 # Establecemos zsh como shell por defecto
 echo "Cambiando el shell predeterminado a Zsh..."
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $USER
 
+####################################
+# Instalamos Nerd Font Jetbrains
+####################################
+
+FONT_NAME="JetBrainsMono"
+FONT_VERSION="v3.0.2" # Cambia esto si hay una nueva versión disponible
+DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/${FONT_NAME}.zip"
+FONT_DIR="$HOME/.local/share/fonts"
+
+# Crear directorio de fuentes si no existe
+mkdir -p "$FONT_DIR"
+
+# Descargar la fuente
+echo "Descargando ${FONT_NAME} Nerd Font..."
+curl -L -o "${FONT_NAME}.zip" "$DOWNLOAD_URL"
+
+# Verificar si la descarga fue exitosa
+if [ $? -ne 0 ]; then
+  echo "Error al descargar la fuente."
+  exit 1
+fi
+
+# Extraer el archivo ZIP
+echo "Extrayendo la fuente..."
+unzip -q "${FONT_NAME}.zip" -d "$FONT_DIR"
+
+# Eliminar el archivo ZIP descargado
+rm "${FONT_NAME}.zip"
+
+# Actualizar la caché de fuentes
+echo "Actualizando la caché de fuentes..."
+fc-cache -fv
+
+# Verificar la instalación
+if fc-list | grep -i "JetBrainsMono" > /dev/null 2>&1; then
+  echo "JetBrains Mono Nerd Font se ha instalado correctamente."
+else
+  echo "Error al instalar JetBrains Mono Nerd Font."
+  exit 1
+fi
+
+###################
 # Instalamos nvim
+###################
+
+# Versión de Neovim a instalar
+NEOVIM_VERSION="v0.10.2"
+
 if command -v nvim >/dev/null 2>&1; then
   echo "Neovim ya instalado."
 else
@@ -65,5 +113,31 @@ else
   fi
 fi
 
+#########################
 # Instalamos oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#########################
+
+# Variables para evitar interacciones durante la instalación
+export RUNZSH=no
+export CHSH=no
+
+# Descargar e instalar Oh My Zsh sin interacción
+echo "Instalando Oh My Zsh de forma silenciosa..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Cambiar el shell por defecto a zsh manualmente sin interacción
+sudo chsh -s $(which zsh) $USER
+
+#########################
+# Fondo de pantalla
+#########################
+
+# Ruta de la imagen que quieres establecer como fondo
+WALLPAPER="oasis-retrowave.png"
+WALLPAPER_PATH="$HOME/dotfiles/wallpapers/$WALLPAPER"
+
+# Cambiar el fondo de pantalla
+gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER_PATH"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER_PATH"  # Para tema oscuro (opcional)
+
+echo "Fondo de pantalla cambiado en GNOME."
