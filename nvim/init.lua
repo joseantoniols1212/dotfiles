b2e89f7dd5d137627ce4b66d693c011d0e44646f
Jose Antonio <joseantonioluquesalguero1212@gmail.com>
2024-11-13 14:20:13 +0100

    nvim file tree keymaps and telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,20 @@
+require("config.lazy")
+
+-- Keymaps config
+vim.g.mapleader = ' '
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
-require("config.lazy")
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Nvimtree
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle file tree' })
